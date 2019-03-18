090b5e86ff59c7c02fcc086747ecad900118bec4
GermanDOK <47220441+GermanDOK@users.noreply.github.com>
2019-03-18 21:56:49 +0300

    Add files via upload

---
+++ subscribe.py
@@ -0,0 +1,10 @@
+import requests 
+from bs4 import BeautifulSoup 
+
+url = "https://realtimeyoutube.com" 
+responce = requests.get(url) 
+
+html = BeautifulSoup(responce.content, "lxml")
+
+def get_subs():
+    return html.find('h2',{"class":"MuiTypography-root-252 MuiTypography-display4-253 t-heading-249"}).h2.text
